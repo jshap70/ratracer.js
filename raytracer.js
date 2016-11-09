6ada2bbc95512f83f59dcbb2997a08aec8b48cd1
fahslaj <fahslaj@rose-hulman.edu>
2016-11-08 23:23:59 -0500

    Implement barycentric technique for triangle intersection

--- raytracer.js
+++ raytracer.js
@@ -18,6 +18,7 @@ function traceRay(pixel) {
     var s = vec3(us, vs, ws);
 
     var minT = Number.POSITIVE_INFINITY;
+    var minObject = undefined;
     for (var i = 0; i < objects.length; i++) {
         var t = objects[i].intersects(eye, s);
         // TODO: do stuff with t
@@ -32,14 +33,34 @@ class Triangle {
     }
 
     intersects(p1, p2) {
-        var v12 = subtract(this.v1, this.v2);
-        var v13 = subtract(this.v1, this.v3);
+        // plane equation: normal (dot) (v3 - v1) = 0
+        //                  normal (dot) v3 = d
+        var v12 = subtract(this.v2, this.v1);
+        var v13 = subtract(this.v3, this.v1);
         var normal = cross(v12, v13);
         var d = dot(normal, this.v3);
         var t = (d - dot(normal, p1)) / dot(normal, subtract(p2, p1));
-        // plane equation: normal (dot) (v3 - v1) = 0
-        //                  normal (dot) v3 = d
-        return t;
+        var p = add(p1, scale(t, subtract(p2, p1)));
+
+        // Barycentric technique
+        // vectors
+        var vec0 = v13;
+        var vec1 = v12;
+        var vec2 = subtract(p, this.v1);
+
+        // dot products
+        var dot00 = dot(vec0, vec0);
+        var dot01 = dot(vec0, vec1);
+        var dot02 = dot(vec0, vec2);
+        var dot11 = dot(vec1, vec1);
+        var dot12 = dot(vec1, vec2);
+
+        // barycentric coords
+        var invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+        var u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+        var v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+        var within = (u >= 0) && (v >= 0) && (u + v < 1);
+        return [within, t, u, v];
     }
 }
 
