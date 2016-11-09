b7972508b46ebcc05c985514ac9ae38356721880
fahslaj <fahslaj@rose-hulman.edu>
2016-11-09 15:58:28 -0500

    Re-oriented axes

--- raytracer.js
+++ raytracer.js
@@ -31,38 +31,46 @@ window.onload = function() {
         pixels.push(row);
     }
 
+    objects.push(new Triangle(vec3(-.1, .4, 1.4), 
+        vec3(-.2, 0, 1.5), vec3(.5, -.2, 1.7)));
+
     window.requestAnimationFrame(main);
 };
 
 function writeToPixel(height, width, color) {
     var pixelindex = (height * canvas.height + width) * 4;
-    imagedata.data[pixelindex]   = color[0] * 255;
-    imagedata.data[pixelindex+1] = color[1] * 255;
-    imagedata.data[pixelindex+2] = color[2] * 255;
+    imagedata.data[pixelindex]   = color[0];
+    imagedata.data[pixelindex+1] = color[1];
+    imagedata.data[pixelindex+2] = color[2];
     imagedata.data[pixelindex+3] = color[3] * 255;
 }
 
 function main() {
     for (var r = 0; r < pixels.length; r++) {
         for (var c = 0; c < pixels[r].length; c++) {
-            var us = (r + .5) * pixelWidth - (frameWidth / 2);
-            var vs = (c + .5) * pixelHeight - (frameHeight / 2);
-            var ws = frameDepth;
-            var s = vec3(us, vs, ws);
-            color = traceRay(s);
+            var s = getS(r, c);
+            var color = traceRay(s);
             if (color != null) {
-                pixels[r][c] = color
+                pixels[r][c] = color;
             }
         }
     }
 
     for (var r = 0; r < pixels.length; r++) {
         for (var c = 0; c < pixels[r].length; c++) {
-            writeToPixel(r, c, pixels[r][c])
+            writeToPixel(c, r, pixels[r][c])
         }
     }
 
     context.putImageData(imagedata, 0, 0);
+}
+
+function getS(r, c) {
+    var us = (r + .5) * pixelWidth - (frameWidth / 2);
+    var vs = (c + .5) * pixelHeight - (frameHeight / 2);
+    var ws = frameDepth;
+    var s = vec3(us, - vs, ws);
+    return s;
 }
 
 function traceRay(s) {
@@ -70,16 +78,15 @@ function traceRay(s) {
     var minObject = undefined;
     for (var i = 0; i < objects.length; i++) {
         var result = objects[i].intersects(eye, s);
-        if (result[0]) {
-            if (result[1] < minT) {
-                minObject = objects[i];
-            }
+        if (result[0] < minT) {
+            minT = result[0];
+            minObject = objects[i];
         }
     }
     var color = undefined;
     if (minT < Number.POSITIVE_INFINITY) {
         // TODO: get color of minObject at this point
-        color = vec3(0, 0, 0);
+        color = vec4(100, 50, 75, 1);
     }
     return color;
 }
